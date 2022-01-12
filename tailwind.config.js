f6022f4dd505d3c3b6e9720679cbbb94d2b2c835
NazarH2ONyzhnyk <nazarh2onyzhnyk@gmail.com>
2022-01-12 14:32:40 +0200

    deploy contract added

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,13 +4,25 @@ module.exports = {
   theme: {
     
     extend: {
-      /*fontFamily: {
-        'JosefinSans': ['JosefinSans', 'sans-serif']
-      },*/
       colors: {
         blue: {
-          900: "#212353"
+          900: "#212353",
+        },
+        red: {
+          500: '#DB2828',
         }
+      },
+      width: {
+        '50': '50px',
+        '61': '60px',
+        '64': '64px',
+        '376': '376px',
+      },
+      height: {
+        '30': '30px',
+        '50': '50px',
+        '64': '64px',
+        '140': '140px',
       }
     },
   },
